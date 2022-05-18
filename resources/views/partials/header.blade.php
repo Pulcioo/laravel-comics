b80550742fc81adfeeabe13b51c820cc82e3ed77
Davide Pulci <davide.pulci@gmail.com>
2022-05-18 17:02:30 +0200

    incluso header al layout base

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="header-container">
+       <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+       <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+        </ul>
+    </div>
+</header>
